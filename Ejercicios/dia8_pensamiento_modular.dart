8623a36390da60f66375bc119d6c2a139b2fd57c
HatiD3v <edwinmg27@gmail.com>
2026-05-04 20:48:01 -0400

    add: implementación de ejercicios sobre funciones y sistema de precios con descuento

--- Ejercicios/dia8_pensamiento_modular.dart
+++ Ejercicios/dia8_pensamiento_modular.dart
@@ -36,4 +36,78 @@ void main() {
   visualizacion(sumar(number1, number2));
 
   //Ejercicio 4 disena funciones> determinar si un numero es par, mostrar el resultado
+  //**
+  //usemos los principios de input, process, output
+  //nuestro input es el numero a determinar si es par
+  //proceso: como determinar si es par
+  //output: mostrar resultado> es par o impar?
+  //Bueno en este ejemplo definimos 2 funciones, 1 que evalua si el numero es par
+  //la segunda muestra el resultado par o impar, pero a este se le paso la funcion evenNumber como parametro(callback)
+  //es decir la funcion procesarNumero recibe como argumento una funcion y un numero entero. el callback valida si el numero es par, y el segundo parametro es el valor entero
+  //que ocurre al llamar a la funcion procesar numero? este le enviamos como argumento la funcion evenNumber y el argumento tipo int, al pasar el numero este es evaluado dentro de la funcion procesarNumero mediante una variable booleana que almacena el valor del callback ya que se debe tener una variable que pueda almacenar ese dato que nos esta procesando la primera funcion
+  //*/
+  bool evenNumber(int even) {
+    return even % 2 == 0;
+  }
+
+  void procesarNumero(bool Function(int) callback, int valor) {
+    bool esPar = callback(valor);
+    if (esPar) {
+      print('es par');
+    } else {
+      print('es impar');
+    }
+  }
+
+  procesarNumero(evenNumber, 12);
+
+  //ejercicio 5 sistema simple
+  //*
+  //calcular precio total
+  //aplicar descuento
+  //validar si el cliente es vip
+  //Dividirlo en funciones
+  //entradas:
+  //ok que tipo de dato vamos a necesitar? el precio del producto
+  // iva a aplicar para el producto
+  //el valor del descuento
+  //saber si el cliente es vip
+  //procesos:
+  //calcular precio total(precio producto + iva)
+  //aplicar descuento al precio total
+  //validar si el cliente es vip si es vip se le aplica descuento
+  //output: mostrar precio total del producto y si el cliente fue vip*/
+  double valorBase = 55;
+  //Obtenemos el precio base
+  double productPrice(double price) => price;
+  //Calculamos el iva(recibe una funcion y un valor)
+  double iva(double Function(double) callback, double price) {
+    return callback(price) * 1.16;
+  }
+
+  // aplicamos descuento sobre un monto ya procesado
+  double amountDiscount(double monto, double porcentaje) {
+    return monto * (1 - porcentaje / 100);
+  }
+
+  void clienteVip({
+    required double precioFinal,
+    required double porcentajeDescuento,
+    required bool esVip,
+  }) {
+    if (esVip) {
+      double finalPrice = amountDiscount(precioFinal, porcentajeDescuento);
+      print(
+        '¡Es VIP! El precio con $porcentajeDescuento% de descuento es: \$${finalPrice.toStringAsFixed(2)}',
+      );
+    } else {
+      print(
+        'No eres VIP. El precio total es: \$${precioFinal.toStringAsFixed(2)}',
+      );
+    }
+  }
+
+  double precioConIva = iva(productPrice, valorBase);
+  clienteVip(precioFinal: precioConIva, porcentajeDescuento: 20, esVip: true);
+  clienteVip(precioFinal: precioConIva, porcentajeDescuento: 20, esVip: false);
 }
