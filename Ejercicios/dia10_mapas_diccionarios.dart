9b4123d02b03081bc5886657307eb6dc44200fa1
HatiD3v <edwinmg27@gmail.com>
2026-05-04 22:17:39 -0400

    add: creación del archivo dia10_mapas_diccionarios.dart con ejercicios sobre mapas

---
+++ Ejercicios/dia10_mapas_diccionarios.dart
@@ -0,0 +1,15 @@
+void main() {
+  /**
+     * Es una estructura que guarda datos en pares key>value
+     * la key identificado al dato
+     * El value es la informacion asociada
+     * Para acceder a los datos es similar como las listas pero sin indice, solo pasando la key
+     * Se pueden insertar datos key > value similar a list con []
+     * Se pueden modificar datos
+     * Eliminar datos
+     * Un mapa se recorre con forEach
+     */
+
+  //Ejercicios progesivos
+  //Ejericicio 1 Traducir a mapa nombre: Carlos edad: 28 activo: true
+}
