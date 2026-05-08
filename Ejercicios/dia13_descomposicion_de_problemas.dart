15c8a1233647e566ac42b5a6a71cf6e63208b5eb
HatiD3v <edwinmg27@gmail.com>
2026-05-08 01:14:53 -0400

    add: implementación del archivo dia13_descomposicion_de_problemas.dart con explicación sobre la descomposición de problemas

---
+++ Ejercicios/dia13_descomposicion_de_problemas.dart
@@ -0,0 +1,23 @@
+void main() {
+  //Que es la descomposicion de problemas? para mi la descomposicion de problemas es ir abstrayendo o desglozando el problema en pequeños pasos hasta hacerlo manejable y poder ver la solucion con claridad.
+  /**
+  Antes de escribir una sola línea de código, debes entender:
+¿Qué me están pidiendo exactamente?
+¿Qué datos tengo?
+¿Qué resultado esperan?
+Ciclo real de un programador
+1. Leer problema
+2. Identificar entrada
+3. Identificar salida
+4. Dividir en pasos
+5. Escribir pseudocódigo
+6. Recién ahí escribir código
+Ahora en este dia veo mas claro lo meticuloso que debemos ser al descomponer. ejemplo numero par o impar los pasos intermedios o procesos serian
+1. Recibir número
+2. Dividir entre 2
+3. Ver si el residuo es 0
+4. Decidir resultado
+Si no puedes explicarlo en pasos simples, no deberías programarlo todavía
+
+   */
+}
