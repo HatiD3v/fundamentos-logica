2392de769348b0e43a4b11db62630d397b02f409
HatiD3v <edwinmg27@gmail.com>
2026-05-11 20:41:15 -0400

    add: creación del archivo dia15_diagramas_de_flujo.dart con explicaciones sobre diagramas de flujo y ejercicios progresivos

---
+++ Ejercicios/dia15_diagramas_de_flujo.dart
@@ -0,0 +1,16 @@
+void main() {
+  /**
+   * Que es un diagrama de flujos?
+   * Un diagrama de flujo es un esquema que representa un algoritmo usando simbolos que se conectan, como inicias, pasos, decisiones, resultado y fin
+   * Simbolos basicos: ovalo representa el inicio y fin. Proceso rectangulo, decision rombo, repeticion se forma cuando una flecha regresa a un paso anterior
+   * Si no puedes seguir el diagrama paso a paso, la lógica está mal
+   * Ejercicios progresiovos
+   * Diagrama para sumar dos numeros
+   * Determinar si un numero es mayor que 8
+   * mostrar numeros del 1 al 10
+   * dado un numero: si es par mostrar par si no impar
+   * dado una lista: recorrer, sumar y mostrar total
+   * 
+   * 
+   */
+}
